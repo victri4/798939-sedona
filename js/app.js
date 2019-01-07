9469c85e056b81158656ca544f774dfb45831b3e
victri4 <44035246+victri4@users.noreply.github.com>
2019-01-07 13:06:19 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
-var form = document.querySelector(".form-search");
-var btn = document.querySelector(".hotel-button");
+(function() {
+    var form = document.querySelector(".form-search");
+    var inputs = form.querySelectorAll('input[type=text]');
+    var btnSubmit = form.querySelector('.search-button');
 
-btn.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    if (form.classList.contains("form-close")) {
-        form.classList.remove("form-close");
-    } else {
-        form.classList.add("form-close");
+    var btn = document.querySelector(".hotel-button");
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        if (typeof(form) != 'undefined' && form != null) {
+            form.classList.remove('no-js');
+        }
+    });
+
+    if (typeof(btn) != 'undefined' && btn != null) {
+        btn.addEventListener("click", function(evt) {
+            evt.preventDefault();
+            form.classList.add('is-showed');
+        });
     }
-})
+
+    if (typeof(btnSubmit) != 'undefined' && btnSubmit != null) {
+        var counter = 0;
+        btnSubmit.addEventListener('click', function(event) {
+            var Form = event.target.form;
+
+            for (var i = 0; i < inputs.length; i++) {
+
+                if (inputs[i].value !== '') {
+                    counter++
+                }
+            }
+            if (counter === inputs.length) {
+                if (form.classList.contains('submit-error')) {
+                    form.classList.remove('submit-error');
+                }
+                Form.submit();
+            } else {
+                event.preventDefault();
+                form.classList.add('submit-error');
+            }
+        });
+    }
+}());
